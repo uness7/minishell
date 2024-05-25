b07a8b6101e51a14518d12db7e90e07c9be30d9c
Youness Zioual <yzioual@c2r12p1.42lehavre.fr>
2024-05-25 15:56:44 +0200

    last stable version of minishell

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: hbenaddi <hbenaddi@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 21:42:27 by hbenaddi          #+#    #+#             */
-/*   Updated: 2024/05/25 13:00:43 by yzioual          ###   ########.fr       */
+/*   Updated: 2024/05/25 15:51:54 by yzioual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,27 @@ static bool	check_str_for_n_char(char *str)
 void	echo_helper(t_echo_arr **echo_args, int *flag)
 {
 	int	i;
+	int	check_flag;
 
+	check_flag = 1;
 	i = 0;
 	while (echo_args[i] != NULL)
 	{
-		if (ft_strncmp(echo_args[i]->data, "-", 1) == 0
+		if (check_flag && ft_strncmp(echo_args[i]->data, "-", 1) == 0
 			&& check_str_for_n_char(echo_args[i]->data) && echo_args[i
 				+ 1] != NULL)
 		{
 			i++;
 			*flag = 1;
 		}
-		if (echo_args[i] && echo_args[i]->type == WORD)
-			printf("%s", echo_args[i]->data);
-		else if (echo_args[i] && echo_args[i]->type == _SPACE)
-			printf(" ");
+		else
+		{
+			check_flag = 0;
+			if (echo_args[i] && echo_args[i]->type == WORD)
+				printf("%s", echo_args[i]->data);
+			else if (echo_args[i] && echo_args[i]->type == _SPACE)
+				printf(" ");
+		}
 		i++;
 	}
 }
